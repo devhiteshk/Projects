66c0c6f9660d3f741d538bfc7409f6282997ec5a
devhiteshk <dev.hitesh.k@gmail.com>
2022-12-16 20:05:37 +0530

    "FCC completed data visualisation and API1"

---
+++ timestamp/index.js
@@ -0,0 +1,57 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+let responseObject = {};
+
+app.get("/api/", function (req, res) {
+  responseObject = {};
+  let r = new Date().getTime();
+  responseObject["unix"] = r;
+  responseObject["utc"] = new Date().toUTCString();
+  res.json(responseObject);
+});
+
+app.get("/api/:date?", function (req, res) {
+  let input = req.params.date;
+  if (input.includes("-")) {
+    let d = new Date(input).getTime();
+    responseObject["unix"] = d;
+    responseObject["utc"] = new Date(input).toUTCString();
+  } else {
+    input = parseInt(input);
+    responseObject["utc"] = new Date(input);
+  }
+
+  if (!responseObject["unix"] || !responseObject["utc"]) {
+    responseObject.json({ error: "invalid date" });
+  }
+
+  res.json(responseObject);
+});
+
+// listen for requests :)
+var listener = app.listen(3000 || process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
